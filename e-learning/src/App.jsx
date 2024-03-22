d07b605f904510c9596179b327a54418e82c7c94
sumit jadhav <sumitjadhav3710@gmail.com>
2024-03-22 10:06:45 +0530

    pages

--- e-learning/src/App.jsx
+++ e-learning/src/App.jsx
@@ -1,6 +1,8 @@
 import Aboutus from './Aboutus'
 import Contactus from './Contactus'
 import QnA from './QandA'
+import Integrate from './Integrate'
+import PlanandPrice from './PlanandPrice'
 
 import './App.css'
 
@@ -10,9 +12,12 @@ function App() {
 
   return (
    <>
+   <PlanandPrice></PlanandPrice>
    <Aboutus></Aboutus>
    <Contactus></Contactus>
    <QnA></QnA>
+   <Integrate></Integrate>
+  
    
    <h1>First</h1>
    <about />
